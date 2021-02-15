79bcf951fabdb02740b01caf679727efab2215cc
Nicolas Halberstadt <halberstadtnicolas@gmail.com>
2021-02-15 15:59:51 +0100

    #3 - Symfony installation

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+/* User: nicolashalberstadt
+* Date: 17/02/2021
+* Time: 11:08
+*/
+
+namespace App\Controller;
+
+/**
+ * Class HomeController
+ *
+ * @author Nicolas Halberstadt <halberstadtnicolas@gmail.com>
+ * @package App\Controller
+ */
+class HomeController
+{
+    
+}

c9d06a942b1610f81721cd2dfff225babedc26a9
Nicolas Halberstadt <halberstadtnicolas@gmail.com>
2021-04-08 16:58:49 +0200

    #10 - page move button management

---
+++ public/js/page-move-btn.js
@@ -0,0 +1,29 @@
+window.onload = function () {
+    handleDisplayToTricksBtn();
+    handleDisplayGoToTopBtn();
+};
+
+let handleDisplayToTricksBtn = function () {
+    window.addEventListener("scroll", function () {
+        let currentPos = (window.pageYOffset || document.documentElement.scrollTop);
+        let btn = $("#tricksBtn");
+        if (currentPos < $("#tricks-container").position().top) {
+            btn.show();
+        } else {
+            btn.hide();
+        }
+    });
+}
+
+let handleDisplayGoToTopBtn = function () {
+    let btn = $("#topBtn");
+    window.addEventListener("scroll", function () {
+        let currentPos = (window.pageYOffset || document.documentElement.scrollTop);
+        let tricksContainer = $("#tricks-container");
+        if (tricksContainer.children().length >= 15 && currentPos > tricksContainer.position().top) {
+            btn.show();
+        } else {
+            btn.hide();
+        }
+    });
+}
